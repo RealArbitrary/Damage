1a6fd306def8421e3c027011afd7f4a74aaa4ebb
DeWet <DeWetvandermerwe@gmail.com>
2026-05-11 21:05:38 +0200

    Allow swagger to be used outside of development environment

--- API/Program.cs
+++ API/Program.cs
@@ -35,14 +35,17 @@ builder.Services.AddSwaggerGen(options =>
 
 var app = builder.Build();
 
+app.UseSwagger();
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    options.RoutePrefix = string.Empty;
+});
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(options => {
-        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
-        options.RoutePrefix = string.Empty;
-    });
+
 }
 
 app.UseHttpsRedirection();
